771aac45f6c02671505713729897d47dc4e8c7f9
Emily <milly@roach.me.uk>
2024-03-18 12:18:20 +0000

    Created the Login component

---
+++ frontend/src/Login.js
@@ -0,0 +1,23 @@
+import {useRef, useState, useEffect} from 'react';
+
+
+const Login = () => {
+    //Refs
+    const userRef = useRef();
+    const errRef = useRef();
+
+    //States
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    //For testing purposes
+    const [success, setSuccess] = useState('');
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Login
