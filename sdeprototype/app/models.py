a13f76a6b4dc0668c02f158c36d6910c3c2cb286
Emily <milly@roach.me.uk>
2024-03-18 10:48:16 +0000

    Unsure what has happened here - but this works...

--- sdeprototype/app/models.py
+++ sdeprototype/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UserInfo(models.Model):
-    userName = models.CharField(max_length=50)
+    userName = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
     userLevel = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
